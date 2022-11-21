69f3f484d1038e0745ba7f99974a3d4027fda623
xansiety <ferando543@outlook.com>
2022-11-21 16:12:51 -0600

    EF -> Creación de entidad Archivos Adjuntos

--- TareasMVC/ApplicationDbContext.cs
+++ TareasMVC/ApplicationDbContext.cs
@@ -18,5 +18,6 @@ namespace TareasMVC
 
         public DbSet<Tarea> Tareas { get; set; }
         public DbSet<Paso> Pasos { get; set; }
+        public DbSet<ArchivoAdjunto> ArchivosAdjuntos { get; set; }
     }
 }
