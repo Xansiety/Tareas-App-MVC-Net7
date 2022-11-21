af529ad2cf8a4daac4b1b789e329351e83680d6e
xansiety <ferando543@outlook.com>
2022-11-21 15:53:38 -0600

    EF -> Se configura base de datos Code Firts y se crear primera entidad

---
+++ TareasMVC/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TareasMVC.Entidades;
+
+namespace TareasMVC
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Tarea> Tareas { get; set; }
+    }
+}
